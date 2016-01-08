f4105b702aee3c91f68cb3d86abb44944bc1a8e7
ridwannaruto <rshariffdeen@gmail.com>
2016-01-08 17:09:58 +0530

    updated methods to calculate cost

--- components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/costmodel/PriceEstimator.java
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/costmodel/PriceEstimator.java
@@ -1,31 +1,49 @@
 package org.apache.stratos.autoscaler.costmodel;
 
 import org.apache.commons.math3.analysis.polynomials.*;
-import org.apache.stratos.autoscaler.context.cluster.ClusterContext;
-import org.apache.stratos.autoscaler.pojo.policy.autoscale.LoadAverage;
-import org.apache.stratos.autoscaler.pojo.policy.autoscale.MemoryConsumption;
+import org.apache.stratos.autoscaler.costmodel.data.InstanceType;
 
 /**
  * Created by ridwan on 1/7/16.
  */
 public class PriceEstimator {
 
-    public float calculateTotalCost(char type,PolynomialSplineFunction polynomial, int instanceCount){
+    private InstanceType instanceType;
+    private PenaltyEstimator penaltyEstimator;
+    public PriceEstimator(String instanceType){
+        this.instanceType = new InstanceType(instanceType);
+        penaltyEstimator = new PenaltyEstimator(this.instanceType);
+    }
+
+    public float calculateTotalCostBasedOnRIF(PolynomialSplineFunction polynomial, int instanceCount){
         float totalCost = 0;
-        switch (type){
-            case GlobalParameters.PERF_MEASURE_TYPE_LA:
+        float acquisitionCost = instanceType.getPerInstanceCost() * instanceCount;
+        float penaltyCost = penaltyEstimator.calculatePenaltyCost(polynomial,instanceCount,CostModelParameters.PERF_MEASURE_TYPE_RIF);
 
-                break;
-            case GlobalParameters.PERF_MEASURE_TYPE_MC:
-
-                break;
-            case GlobalParameters.PERF_MEASURE_TYPE_RIF:
-
-                break;
-        }
-
-
+        totalCost = acquisitionCost + penaltyCost;
         return totalCost;
     }
+
+    public float calculateTotalCostBasedOnLA(PolynomialSplineFunction polynomial, int instanceCount){
+        float totalCost = 0;
+        float acquisitionCost = instanceType.getPerInstanceCost() * instanceCount;
+        float penaltyCost = penaltyEstimator.calculatePenaltyCost(polynomial,instanceCount,CostModelParameters.PERF_MEASURE_TYPE_LA);
+
+        totalCost = acquisitionCost + penaltyCost;
+        return totalCost;
+    }
+
+
+    public float calculateTotalCostBasedOnMC(PolynomialSplineFunction polynomial, int instanceCount){
+        float totalCost = 0;
+        float acquisitionCost = instanceType.getPerInstanceCost() * instanceCount;
+        float penaltyCost = penaltyEstimator.calculatePenaltyCost(polynomial,instanceCount,CostModelParameters.PERF_MEASURE_TYPE_MC);
+
+        totalCost = acquisitionCost + penaltyCost;
+        return totalCost;
+    }
+
+
+
 
 }
