738f6059049e260cca75ff0fcf11085a16bb354b
ridwannaruto <rshariffdeen@gmail.com>
2016-01-15 19:09:29 +0530

    logs added

--- components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/costmodel/CostEstimator.java
+++ components/org.apache.stratos.autoscaler/src/main/java/org/apache/stratos/autoscaler/costmodel/CostEstimator.java
@@ -1,5 +1,7 @@
 package org.apache.stratos.autoscaler.costmodel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.commons.math3.analysis.polynomials.*;
 import org.apache.stratos.autoscaler.costmodel.data.InstanceSpec;
 
@@ -10,6 +12,8 @@ public class CostEstimator {
 
     private InstanceSpec instanceType;
     private PenaltyEstimator penaltyEstimator;
+    private static final Log log = LogFactory.getLog(CostEstimator.class);
+
     public CostEstimator(String instanceType, String regionName){
         this.instanceType = new InstanceSpec(instanceType,regionName);
         penaltyEstimator = new PenaltyEstimator(this.instanceType);
@@ -21,6 +25,8 @@ public class CostEstimator {
         float penaltyCost = penaltyEstimator.calculatePenaltyCost(polynomial,instanceCount,CostModelParameters.PERF_MEASURE_TYPE_RIF);
 
         totalCost = acquisitionCost + penaltyCost;
+
+        log.info("Cost estimated using RIF for " + instanceCount + " instances: " + acquisitionCost + " + " + penaltyCost);
         return totalCost;
     }
 
@@ -30,6 +36,8 @@ public class CostEstimator {
         float penaltyCost = penaltyEstimator.calculatePenaltyCost(polynomial,instanceCount,CostModelParameters.PERF_MEASURE_TYPE_LA);
 
         totalCost = acquisitionCost + penaltyCost;
+        log.info("Cost estimated using LA for " + instanceCount + " instances: " + acquisitionCost + " + " + penaltyCost);
+
         return totalCost;
     }
 
@@ -40,6 +48,8 @@ public class CostEstimator {
         float penaltyCost = penaltyEstimator.calculatePenaltyCost(polynomial,instanceCount,CostModelParameters.PERF_MEASURE_TYPE_MC);
 
         totalCost = acquisitionCost + penaltyCost;
+        log.info("Cost estimated using MC for " + instanceCount + " instances: " + acquisitionCost + " + " + penaltyCost);
+
         return totalCost;
     }
 
